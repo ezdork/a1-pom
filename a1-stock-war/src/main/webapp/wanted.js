a0bd8de0428881b42cb3add348dca1a9df1a6af7
timezstyle <timezstyle@gmail.com>
2013-11-22 08:01:37 +0800

    計算 淨利

--- a1-stock-war/src/main/webapp/wanted.js
+++ a1-stock-war/src/main/webapp/wanted.js
@@ -27,14 +27,18 @@ function getWantedStockList(date, clearCache, event) {
 function appendCurrentBuyTable(tableId, data) {
 	var msg = '持有股票';
 	$('#content').append(
-			'<table id="' + tableId + '" ><THEAD><tr><td colspan="7">' + msg
-					+ '</td></tr><tr><td>股票代號</td><td>5日均</td><td>跌停價</td><td>張數</td><td>購買日期</td><td>購買價</td><td>現價</td></tr></THEAD></table>');
+			'<table id="' + tableId + '" ><THEAD><tr><td colspan="9">' + msg
+					+ '</td></tr><tr><td>股票代號</td><td>5日均</td><td>跌停價</td><td>張數</td><td>購買日期</td><td>購買價</td><td>現價</td><td>淨利</td><td>手續費</td></tr></THEAD></table>');
 	var $table = $('#' + tableId);
 	var list = data[tableId];
 	var length = list.length;
 	$table.append('<TBODY>');
 	for ( var i = 0; i < length; i++) {
-		var html = list[i]['nowPrice'] > list[i]['buyPrice'] ? '<tr style="color:red">' : '<tr style="color:green">';
+		
+		var earnMoney = list[i]['buyAmount'] * Math.floor((list[i]['nowPrice'] - list[i]['buyPrice']) * 1000);
+		var fee = list[i]['buyAmount'] * list[i]['nowPrice'] * 1.425 + list[i]['buyAmount'] * list[i]['nowPrice'] * 4.425;
+		
+		var html = (earnMoney - fee)>0 ? '<tr style="color:red">' : '<tr style="color:green">';
 		html += '<td>' + list[i]['code'] + '</td>';
 		html += '<td>' + list[i]['ma5'] + '</td>';
 		html += '<td>' + list[i]['lowestPrice'] + '</td>';
@@ -42,6 +46,8 @@ function appendCurrentBuyTable(tableId, data) {
 		html += '<td>' + list[i]['buyDate'] + '</td>';
 		html += '<td>' + list[i]['buyPrice'] + '</td>';
 		html += '<td>' + list[i]['nowPrice'] + '</td>';
+		html += '<td style="text-align:right">' + accounting.formatMoney(earnMoney - fee) + '</td>';
+		html += '<td style="text-align:right">' + accounting.formatMoney(fee) + '</td>';
 		html += '</tr>';
 		$table.append(html);
 	}
@@ -68,6 +74,22 @@ function appendTable(tableId, data) {
 }
 
 $(function() {
+	accounting.settings = {
+			currency : {
+				symbol : "$", // default currency symbol is '$'
+				format : "%s%v", // controls output: %s = symbol, %v =
+				// value/number (can be object: see below)
+				decimal : ".", // decimal point separator
+				thousand : ",", // thousands separator
+				precision : 0
+			// decimal places
+			},
+			number : {
+				precision : 0, // default precision on numbers is 0
+				thousand : ",",
+				decimal : "."
+			}
+		};
 	$.getJSON('getLatestStockDate.do', function(data) {
 		if (data) {
 			$('#date').val(data);

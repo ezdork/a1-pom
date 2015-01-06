d92211a9cb818643ea9fd282077f34d9a1a5c1c5
tim <ez.dork@gmail.com>
2015-01-06 09:59:01 +0800

    move to 10.0.1.5

--- a1-stock-server/src/test/java/ez/dork/stock/util/GetStockTest.java
+++ a1-stock-server/src/test/java/ez/dork/stock/util/GetStockTest.java
@@ -12,8 +12,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import ez.dork.stock.service.StockService;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "file:/Users/tim/Documents/workspace/a1-pom/a1-stock-war/src/main/webapp/WEB-INF/spring/servlet-context.xml" })
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = { "file:/Users/tim/Documents/workspace/a1-pom/a1-stock-war/src/main/webapp/WEB-INF/spring/servlet-context.xml" })
 public class GetStockTest {
 
 	private static final SimpleDateFormat yyyyMMdd = new SimpleDateFormat(
@@ -24,21 +24,21 @@ public class GetStockTest {
 
 	@Test
 	public void test() {
-		Calendar from = Calendar.getInstance();
-		from.set(2014, 4, 1);
-
-		Calendar to = Calendar.getInstance();
-		 to.set(2014, 4, 1);
-		while (to.compareTo(from) >= 0) {
-			long start = new Date().getTime();
-			service.insertGovStock(to);
-			service.insertOrgStock(to);
-
-			long end = new Date().getTime();
-			System.out.println(yyyyMMdd.format(to.getTime()) + " : "
-					+ (end - start));
-			to.add(Calendar.DAY_OF_MONTH, -1);
-		}
+//		Calendar from = Calendar.getInstance();
+//		from.set(2014, 8, 1);
+//
+//		Calendar to = Calendar.getInstance();
+//		to.set(2014, 8, 1);
+//		while (to.compareTo(from) >= 0) {
+//			long start = new Date().getTime();
+//			service.insertGovStock(to);
+//			service.insertOrgStock(to);
+//
+//			long end = new Date().getTime();
+//			System.out.println(yyyyMMdd.format(to.getTime()) + " : "
+//					+ (end - start));
+//			to.add(Calendar.DAY_OF_MONTH, -1);
+//		}
 
 	}
 }
